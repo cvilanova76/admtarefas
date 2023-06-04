8bb377d309e09f224c44ec318608c50017913f47
cvilanova <carolinavilanova@gmail.com>
2023-06-04 18:53:07 -0300

    first commit

---
+++ src/main/java/br/com/ccvn/admtarefas/AdmtarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ccvn.admtarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmtarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmtarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ccvn/admtarefas/AdmtarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ccvn.admtarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmtarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
